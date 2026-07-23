935c1a21e04f30dc9ccb0e774c7609a6fa692f7a
Abdoae35 <Abdelrahman3535.elsheref@gmail.com>
2026-07-23 21:03:49 +0300

    Refactor Fine class constructor to initialize properties

--- Fine.cs
+++ Fine.cs
@@ -1,6 +1,10 @@
 public class Fine
 {
-     
+     public Fine(string plateNumber,double totalFineAmount)
+     {
+          this.plateNumber = plateNumber;
+          this.totalFineAmount = totalFineAmount;
+     }
      public string? plateNumber { get; set; }
      public double totalFineAmount { get; set; }
 }
