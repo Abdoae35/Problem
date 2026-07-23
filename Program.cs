fe15e5ece64f2e40b6a857e6f344f7a625899ac3
Abdoae35 <Abdelrahman3535.elsheref@gmail.com>
2026-07-23 20:07:24 +0300

    Add NuGet configuration and project assets for Problem project
    - Created Problem.csproj.nuget.g.props to define NuGet properties and restore settings.
    - Added Problem.csproj.nuget.g.targets for build targets related to NuGet.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache to store cache information for NuGet packages.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleRadar;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+      
+}
+
---
+++ Vehicle.cs
@@ -0,0 +1,33 @@
+namespace VehicleRadar;
+
+public class Vehicle
+{
+
+    //constractor
+    public Vehicle(string? plateNumber, DateTime? date, VehicleType? vehicleType, double speed, bool seatBelt)
+    {
+        this.plateNumber = plateNumber;
+        this.date = date;
+        this.vehicleType = vehicleType;
+        this.speed = speed;
+        this.seatBelt = seatBelt;
+    }
+
+   public string? plateNumber { get; set; }
+   public DateTime? date { get; set; }
+   public VehicleType? vehicleType { get; set; }
+   public double speed { get; set; }
+   public bool seatBelt { get; set; } 
+
+
+
+    
+}
+
+public enum VehicleType
+{
+    Private,
+    Truck,
+    Bus
+    
+}
